67d00f5b637bc60e9c54d14224f50f84d36f1b6d
Edwin Zhao <contact@edwinzhao.com>
2025-04-05 19:16:13 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<button class="btn btn-primary">Hello daisyUI</button>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+	:global(html) {
+		background-color: theme(--color-gray-100);
+	}
+</style>
